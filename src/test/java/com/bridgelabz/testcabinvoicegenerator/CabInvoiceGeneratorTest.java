feebd9e003980589f75c864b8d583b59adbc58d6
Subhamdas20 <subham21.sd@gmail.com>
2021-11-22 20:28:51 +0530

    Added :uc1 to  Calculated fare

---
+++ src/test/java/com/bridgelabz/testcabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.testcabinvoicegenerator;
+
+import com.bridgelabz.cabinvoice.CabInvoiceGenerator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CabInvoiceGeneratorTest {
+    @Test
+    public void givenDistanceAndTimeWhenComputedShouldReturnTotalFare(){
+        CabInvoiceGenerator invoice = new CabInvoiceGenerator();
+        double distance=10,time =1;
+        double result = invoice.CalculateFare( distance , time);
+        Assert.assertEquals(100,result,0);
+    }
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare(){
+        CabInvoiceGenerator invoice = new CabInvoiceGenerator();
+        double distance=0.1,time =1;
+        double result = invoice.CalculateFare( distance , time);
+        Assert.assertEquals(5,result,0);
+    }
+}
